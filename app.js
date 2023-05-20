c98a36ada1446b33edabd9972afe5a1ec32d6a3c
Karen Tsaturyan <kar_en_2002@mail.ru>
2023-05-20 21:21:49 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import {faker} from '@faker-js/faker';
+import _ from 'lodash'
+
+const dataLength = 10
+const users = []
+
+for (let i = 0; i < dataLength; i++) {
+  users.push({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    phone: faker.phone.number(),
+  })
+}
+
+console.log(users)
+
+const sortByNames = _.orderBy(users, ['name'], ['asc']);
+
+console.log(sortByNames)
+
+
