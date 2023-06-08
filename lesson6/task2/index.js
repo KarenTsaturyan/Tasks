a369701878d5abbeae901471e0d73f7153214e42
Karen <106877481+KarenTsaturyan@users.noreply.github.com>
2023-06-08 13:52:48 +0400

    Update index.js

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -3,43 +3,28 @@ import csv from 'csv-parser';
 import fs from 'fs';
 import cluster from 'cluster';
 
-
-let results = {}
-
- process.stdin.on('data', (data)=>{
-    if(!data.toString().trim()){
-        console.log('Invalid input')
-    }else{
-        CSVParse(data.toString().trim())
-    }
-
-})
-
 function CSVParse(dirname){
     if(cluster.isPrimary){//main process
-        fs.readdir(dirname, function(err, filenames){//'./data/'
-            if (err) {
-              console.log(err);
-              return;
-            }
-            filenames.forEach(function(filename) {
-                let file = filename.split('.')
-                let arr = []
-                
-                if(file[1] === 'csv'){
-                // const writeStream= fs.createWriteStream(`./dir/${file.join('.')}` );
-                    fs.createReadStream(dirname + file.join('.'))
-                    .pipe(csv())//parsing to JSON
-                    .on('data', (data) => arr.push(data))
-                    .on('end', () => {
-                        results[`${file.join('.')}`] = arr 
-                    })
-                    // .pipe(writeStream)
-                }
-            });
-                    console.log(results);
-          });
+        fs.readdirSync(dirname).forEach(function(filename) {
+                    let file = filename.split('.')
+                    let arr = []
+                    if(file[1] === 'csv'){
+                        const writeStream = fs.createWriteStream(`./dir/${file[0]}.json`);
+                        fs.createReadStream(dirname + file.join('.'))
+                        // .pipe(operationName)
+                        .pipe(csv())//parsing to obj
+                        // .on('data', (data) => console.log(data))
+                        // .on('data', (data) => arr.push(data))
+                        .pipe(writeStream)
+                    }
+                });
         }else{//worker process
            
         }
+}
+
+if(process.argv[2] === undefined){
+    console.log('No args given');
+}else{
+        CSVParse(process.argv[2])
 }
