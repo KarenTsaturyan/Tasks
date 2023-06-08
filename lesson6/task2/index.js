ed487975c6b4b2c272e367ba942386df260b7de5
Karen <106877481+KarenTsaturyan@users.noreply.github.com>
2023-06-08 23:07:57 +0400

    add CLUSTERS version

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -3,33 +3,63 @@ import csv from 'csv-parser';
 import fs from 'fs';
 import cluster from 'cluster';
 
-let csvCount = 0
+// let worker;
+// let csvCount = 0
 let otherFileTypes = 0;
+
 function CSVParse(dirname){
-    if(cluster.isPrimary){//main process
-        fs.readdirSync(dirname).forEach(function(filename) {
-                    let file = filename.split('.')
-                    let arr = []
-                    if(file[1] === 'csv'){
-                        const writeStream = fs.createWriteStream(`./dir/${file[0]}.json`);
-                        fs.createReadStream(dirname + file.join('.'))
-                        .pipe(csv())//parsing to JSON
-                        .on('data', (data) => arr.push(data))
-                        .on('end', () => 
-                        // .pipe(writeStream)
-                         fs.writeFileSync(`./dir/${file[0]}.json`, JSON.stringify(arr))
-                         )
-                         csvCount++;
-                        }else{
-                            otherFileTypes++
-                        }
-                    });
-                    console.log('otherFiles = '+otherFileTypes);
-                    console.log("csvCount = "+csvCount);
-            }else{//worker process
-                
-            }
+
+    
+      
+      
+      if (cluster.isPrimary) {
+        let csvArr = fs.readdirSync(dirname).filter(el=>el.split('.')[1]==='csv')
+        console.log('Master ' + process.pid + ' has started.');
+      
+        // Fork workers.
+        for (var i = 0; i < 2; i++) {
+          var worker = cluster.fork();
+      
+          // Receive messages from this worker and handle them in the master process.
+          worker.on('message', function(msg) {
+          worker.send({files:csvArr, msgFromMaster: 'This is from master ' + process.pid + ' to worker ' + worker.pid + '.'});
+
+            console.log('Master ' + process.pid + ' received message from worker .', msg.csv);
+          });
+      
+          // Send a message from the master process to the worker.
+          // worker.send({files:csvArr, msgFromMaster: 'This is from master ' + process.pid + ' to worker ' + worker.pid + '.'});
         }
+      
+        // Be notified when worker processes die.
+        cluster.on('death', function(worker) {
+          console.log('Worker ' + worker.pid + ' died.');
+        });
+      
+      }else {
+        // let csvCount = 0
+        console.log('Worker ' + process.pid + ' has started.');
+      
+        // Receive messages from the master process.
+        process.on('message', function(msg) {
+            console.log('Worker ' + process.pid + ' received message from master.', msg);
+            let arr = []
+            msg.files.forEach(function(filename) {
+                let file = filename.split('.')
+                fs.createReadStream(dirname + file.join('.'))
+                .pipe(csv())//parsing to JSON
+                .on('data', (data) => arr.push(data))
+                .on('end', () => 
+                fs.writeFileSync(`./dir/${file[0]}.json`, JSON.stringify(arr))
+                )
+            })
+        });
+        // Send message to master process.
+        process.send({msgFromWorker: 'This is from worker ' + process.pid + '.'})
+        
+      }
+
+}
         
         if(process.argv[2] === undefined){
             console.log('No args given');
