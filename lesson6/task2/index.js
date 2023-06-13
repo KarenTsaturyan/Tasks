5a3a0b6957251a6759eb05313b6e5b152f6db186
Karen <106877481+KarenTsaturyan@users.noreply.github.com>
2023-06-14 00:02:08 +0400

    distribute the csv file paths evenly among workers, added records count

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -2,61 +2,84 @@
 import csv from 'csv-parser';
 import fs from 'fs';
 import cluster from 'cluster';
+import  os,{ availableParallelism } from 'os'
 
-// let worker;
-// let csvCount = 0
-let otherFileTypes = 0;
+// subarrays
+function sliceIntoSubArr(arr, size){
+  let res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    const subArr = arr.slice(i, i+size);
+    res.push(subArr) 
+  }
+  return res
+}
 
 function CSVParse(dirname){
-
-    
-      
-      
-      if (cluster.isPrimary) {
+  let recordsCount = 0
+  let csvCount = 0
+  
+  if (cluster.isPrimary) {
+        let newCsvArr;
         let csvArr = fs.readdirSync(dirname).filter(el=>el.split('.')[1]==='csv')
+        
+        if(availableParallelism()>csvArr.length){
+           newCsvArr = sliceIntoSubArr(csvArr, 1)
+        }else{
+           newCsvArr = sliceIntoSubArr(csvArr, Math.ceil(csvArr.length/availableParallelism()))
+        }
+        // console.log(newCsvArr);
         console.log('Master ' + process.pid + ' has started.');
       
         // Fork workers.
-        for (var i = 0; i < 2; i++) {
-          var worker = cluster.fork();
-      
+        for (let i = 0; i < availableParallelism(); i++) {
+          let worker = cluster.fork();
+
           // Receive messages from this worker and handle them in the master process.
           worker.on('message', function(msg) {
-          worker.send({files:csvArr, msgFromMaster: 'This is from master ' + process.pid + ' to worker ' + worker.pid + '.'});
-
-            console.log('Master ' + process.pid + ' received message from worker .', msg.csv);
+            // Send a message from the master process to the worker.
+            // console.log(msg?.count);
+            if(msg.msgFromWorker){
+              worker.send({files:newCsvArr[i], msgFromMaster: 'This is from master ' + process.pid + ' to worker ' + worker.pid + '.'});
+            }
+            if(msg.count){
+              recordsCount += msg.count
+            }
+              console.log('Master ' + process.pid + ' received message from worker .', msg.msgFromWorker);
+              console.log(`RecordsCount in all files ${recordsCount}`);            
           });
-      
-          // Send a message from the master process to the worker.
-          // worker.send({files:csvArr, msgFromMaster: 'This is from master ' + process.pid + ' to worker ' + worker.pid + '.'});
         }
-      
+
+        
         // Be notified when worker processes die.
         cluster.on('death', function(worker) {
           console.log('Worker ' + worker.pid + ' died.');
         });
       
       }else {
-        // let csvCount = 0
         console.log('Worker ' + process.pid + ' has started.');
-      
         // Receive messages from the master process.
         process.on('message', function(msg) {
-            console.log('Worker ' + process.pid + ' received message from master.', msg);
-            msg.files.forEach(function(filename) {
-              let arr = []
+            // console.log('Worker ' + process.pid + ' received message from master.', msg);
+            msg?.files?.forEach(function(filename) {
+                let arr = [];
                 let file = filename.split('.')
                 fs.createReadStream(dirname + file.join('.'))
                 .pipe(csv())//parsing to JSON
-                .on('data', (data) => arr.push(data))
-                .on('end', () => 
-                fs.writeFileSync(`./dir/${file[0]}.json`, JSON.stringify(arr))
+                .on('data', (data) =>{
+                  arr.push(data);
+                  ++csvCount
+                })
+                .on('end', () => {
+                  fs.writeFile(`./dir/${file[0]}.json`, JSON.stringify(arr), (err)=>console.log(err))
+                  process.send({count: csvCount})
+                }
                 )
             })
         });
-        // Send message to master process.
-        process.send({msgFromWorker: 'This is from worker ' + process.pid + '.'})
+
         
+        // Send message to master process.
+       process.send({msgFromWorker: 'This is from worker ' + process.pid + '.'})
       }
 
 }
