22e7e1175acbcfd644ad1d22a7f760ce06d12e11
Karen <106877481+KarenTsaturyan@users.noreply.github.com>
2023-06-27 16:33:42 +0400

    Update app.ts

--- Lesson10/src/app.ts
+++ Lesson10/src/app.ts
@@ -1,155 +1,109 @@
 import {Worker, isMainThread, parentPort, workerData,} from 'worker_threads'
 import fs from 'fs'
-import { availableParallelism } from 'os';
 import http, { ServerResponse } from 'http'
 import url from 'url'
 import { TypeError, TypeToWorkerMessage } from './Types';
-
-
-function sliceIntoSubArr(arr:string[], size:number):string[][]{//[[],[],[],...]
-    let res = [];
-    for (let i = 0; i < arr.length; i += size) {
-      const subArr:string[] = arr.slice(i, i+size);
-      res.push(subArr) 
-    }
-    return res
-  }
+import parseJSAsync from './helpers/csvParser'
+import { sendResponse } from './helpers/res';
   
 if (isMainThread) {
-  const server = http.createServer((req:http.IncomingMessage, res:http.ServerResponse) => {
+    const server = http.createServer((req:http.IncomingMessage, res:http.ServerResponse) => {
     const reqUrl:string|null = url.parse(req.url as string).pathname
-    // console.log(reqUrl!.split('/'))
     //Errors
-    req.on('error', (err:TypeError) => {
-      console.error(err);
-      // Handle error...
-      res.statusCode = 400;
-      res.end('400: Bad Request');
-      return;
-  });
+          req.on('error', (err:TypeError) => {
+            console.error(err);
+            // Handle error...
+            res.statusCode = 400;
+            res.end('400: Bad Request');
+            return;
+          });
 
-  res.on('error', (err:TypeError) => {
-      console.error(err);
-      // Handle error...
-  });
+          res.on('error', (err:TypeError) => {
+              console.error(err);
+              sendResponse(res, '502 bad Gateway')
+          });
+
   if(typeof reqUrl === 'string'){
-      if (reqUrl == "/") {//main path
-        res.write('hello world')
-        return res.end();
-      }
+      if (reqUrl == "/") sendResponse(res, 'hello');
+
       if (req.method == "GET") {//GET method
-        let body:string
-        console.log('Get', reqUrl);
-      if (reqUrl == "/files") {
-            req.on('data', function (data:BufferSource) {
-            body=String(data).trim().split(' ')[0]
-            })
-            req.on('end',()=>{
-              fs.readdir(`../${body}/`,(err:TypeError, files:string[]) => {
-                if (err){
-                  console.log(err);
-                }else {
-                  console.log("\nCurrent directory filenames:");
-                  files.forEach((file:string) => {
-                    res.write(`${file}\n`);
-                    console.log(file);
+            let body:string
+            console.log('Get', reqUrl);
+            if (reqUrl == "/files") {
+                  req.on('data', function (data:BufferSource) {
+                    body=String(data).trim().split(' ')[0]
                   })
-                  res.end('finished')
-                }
-              })
-            })  
-        }else if(reqUrl == `/files/${reqUrl.split('/')[2]}` && reqUrl.split('/')[2].trim() !==''){
-          req.on('data', function (data:BufferSource) {
-            body=String(data).trim().split(' ')[0]
-          })
-          req.on('end', ()=>{
-            fs.readFile(`../${body}/${reqUrl.split('/')[2]}`,(err:TypeError, data:BufferSource) => {
-              if (err) throw err;
-              // console.log(data.toString());
-              res.write(data.toString())
-              res.end('finished')
-            });
-          })
-        }else{
-          res.write('Nothing has been sent')
-          res.end()
-        }
-    } else if (req.method == "POST") {//POST method
-      console.log('POST', reqUrl);
-        if (reqUrl == "/exports") {
-          let body:string = '';
-    
-          req.on('data', function (data:BufferSource) {
-              body = String(data).trim().split(' ')[0];// first word of raw data
-          })
-          req.on("end",()=>{
-            console.log(body);
-      
-            parseJSAsync("../data/", body).then(count=>{
-              console.log(`records=>${count}`)
-            })//body = dir
-            console.log("POST END");     
-            res.write('Files have been created')
-            return res.end();
-        });
-        }
+                  req.on('end',()=>{
+                    fs.readdir(`../${body}/`,(err:TypeError, files:string[]) => {
+                      try{
+                        console.log("\nCurrent directory filenames:");
+                          files.forEach((file:string) => {
+                            res.write(`${file}\n`);
+                            console.log(file);
+                          })
+                          res.end('finished')
+                      }catch(err){
+                          console.log('readdirErr',err);
+                          sendResponse(res, 'directory not found')
+                      }
+                    })
+                  })  
+              }else if(reqUrl == `/files/${reqUrl.split('/')[2]}` && reqUrl.split('/')[2].trim() !==''){
+                req.on('data', function (data:BufferSource) {
+                  body=String(data).trim().split(' ')[0]
+                })
+                req.on('end', ()=>{
+                  fs.readFile(`../${body}/${reqUrl.split('/')[2]}`,(err:TypeError, data:BufferSource) => {
+                    if (err){
+                      console.log('readFileError',err);
+                      sendResponse(res, 'Wrong directory or filename')
+                      return
+                    } 
+                    // console.log(data.toString());
+                    sendResponse(res, data.toString())
+                  });
+                })
+              }else{
+                sendResponse(res, 'Nothing has been sent')
+              }
+
+    }else if (req.method == "POST") {//POST method
+            console.log('POST', reqUrl);
+              if (reqUrl == "/exports") {
+                let body:string = '';
+          
+                req.on('data', function (data:BufferSource) {
+                    body = String(data).trim().split(' ')[0];// first word of raw data
+                })
+                req.on("end",()=>{
+                  // console.log(body);
+                      parseJSAsync("../data/", body).then(count=>{
+                        console.log(`records=>${count}`)
+                        sendResponse(res, 'Files have been created')
+                      }).catch(err=>{
+                        console.log('postErr',err)
+                        sendResponse(res, 'no such a file or directory')
+                      });
+              });
+              }
     }else if (req.method == "DELETE") {//DELETE method
       let body:string;
-      req.on('data', function (data:BufferSource) {
-        body=String(data).trim().split(' ')[0]
-      })
-      req.on('end',()=>{
-        if(reqUrl == `/files/${reqUrl.split('/')[2]}` && reqUrl.split('/')[2].trim() !==''){
-          fs.unlink(`../${body}/${reqUrl.split('/')[2]}`,function(err:TypeError):ServerResponse | void{
-            if(err) return console.log(err);
-            console.log('file deleted successfully');
-            res.write(`${reqUrl.split('/')[2]} deleted successfully\n`);
-            return res.end();
-          });  
-        }
-      })
+        req.on('data', function (data:BufferSource) {
+          body=String(data).trim().split(' ')[0]
+        })
+        req.on('end',()=>{
+            if(reqUrl == `/files/${reqUrl.split('/')[2]}` && reqUrl.split('/')[2].trim() !==''){
+              fs.unlink(`../${body}/${reqUrl.split('/')[2]}`,function(err:TypeError):ServerResponse | void{
+                if(err){
+                  // console.log(err);
+                  sendResponse(res, 'Wrong directory or filename')
+                } 
+                  sendResponse(res, `${reqUrl.split('/')[2]} deleted successfully\n`)
+              });  
+            }
+        })
     }
   }
   })
-  // CSV-JSON Parser
-    function parseJSAsync(dirname:string, newDir:string): Promise<number> {
-        let newCsvArr:string[][];
-        let csvArr:string[] = fs.readdirSync(dirname).filter((el:string)=>el.split('.')[1]==='csv')
-        let cpusCore = availableParallelism()
-        if(availableParallelism()>csvArr.length){
-                   newCsvArr = sliceIntoSubArr(csvArr, 1)
-                   cpusCore = csvArr.length
-                }else{
-                   newCsvArr = sliceIntoSubArr(csvArr, Math.ceil(csvArr.length/availableParallelism()))
-                }
-      return new Promise((resolve, reject): void => {
-        let res:number = 0
-            let arr:number[]=[]
-
-        for (let n = 0; n < cpusCore; n++){
-            const worker = new Worker('./worker.js' );
-           let arrSlice:string[] = newCsvArr[n]
-            worker.postMessage({files:arrSlice, dirname:dirname, newDir:newDir});
-            worker.on('message', (msg:TypeToWorkerMessage): void=>{
-                // console.log(msg.csvCount);
-                arr.push(msg.csvCount)
-                res += msg.csvCount
-                // console.log(arr);
-                if(arr.length>=csvArr.length){
-                  resolve(res)
-                  // process.exit()
-                }
-              })
-              worker.on('error', reject);//{terminate}
-              worker.on('exit', (code:number):void => {
-              // worker.terminate()
-                if (code !== 0)
-                reject(new Error(`Worker stopped with exit code ${code}`));
-            });
-        }
-      });
-    };
     server.listen(3000)
   }
-
-
